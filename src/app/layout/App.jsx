b2bd0eec36e5226ec3d74a2b3ae5d4b3539a2093
DenilsonDesigns <daniellanguilller@gmail.com>
2019-01-24 20:58:00 +1100

    first commit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
+import EventDashboard from "./../../features/events/EventDashboard/EventDashboard";
+import Navbar from "./../../features/nav/NavBar/Navbar";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default App;
